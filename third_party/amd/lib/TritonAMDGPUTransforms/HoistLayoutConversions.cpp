d5b0653cceef86b90742b8a3db9c3a3321383ec9
Paul Trojahn <paul.trojahn@amd.com>
2025-06-12 12:03:29 +0200

    Revert "WIP"
    This reverts commit f8b456b51f12021d825ba40b4069fd8a0372f284.

--- third_party/amd/lib/TritonAMDGPUTransforms/HoistLayoutConversions.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/HoistLayoutConversions.cpp
@@ -18,9 +18,6 @@ namespace {
 // we want to load Q tensor and keep it in register, instead of loading it
 // (neither from global or shared memory) at every iteration of the loop.
 static void hoistCvtDotOpOutOfLoop(ttg::ConvertLayoutOp cvtOp) {
-  OpBuilder b(cvtOp);
-  //ttg::BlockedEncodingAttr::get(ctx, shape, newSizePerThread, order,
-  //                                     numWarps, threadsPerWarp, numCTAs);
   // Check the dst of cvt has dotOperand layout
   RankedTensorType rtType = dyn_cast<RankedTensorType>(cvtOp.getType());
   if (!rtType)
@@ -36,26 +33,6 @@ static void hoistCvtDotOpOutOfLoop(ttg::ConvertLayoutOp cvtOp) {
     scf::ForOp parentForOp = cvtOp->getParentOfType<scf::ForOp>();
     if (parentForOp && !parentForOp->isAncestor(srcDefOp)) {
       cvtOp->moveAfter(srcDefOp);
-
-      // Add an additional transformation right before the use to create the lane duplication
-      auto dotOpLayoutDst = cast<mlir::triton::gpu::DotOperandEncodingAttr>(
-          cast<RankedTensorType>(cvtOp.getResult().getType()).getEncoding());
-      auto wmmaLayoutDst = cast<mlir::triton::gpu::AMDWmmaEncodingAttr>(dotOpLayoutDst.getParent());
-      ttg::AMDWmmaEncodingAttr intermediateEncoding =
-        ttg::AMDWmmaEncodingAttr::get(wmmaLayoutDst.getContext(), 1, false, wmmaLayoutDst.getWarpsPerCTA(), wmmaLayoutDst.getCTALayout());
-
-      // Convert to intermediate format first
-      auto dotOpType = cast<RankedTensorType>(cvtOp.getResult().getType());
-      auto newType = dotOpType.cloneWithEncoding(intermediateEncoding);
-      b.setInsertionPoint(cvtOp);
-      auto cvtOpNew = b.create<ttg::ConvertLayoutOp>(cvtOp.getLoc(), newType, srcDefOp->getResult(0));
-
-      // Add convert to the dot type for all uses
-      for (auto &use : cvtOp.getResult().getUses()) {
-        b.setInsertionPoint(use.getOwner());
-        auto dotOpValue = b.create<ttg::ConvertLayoutOp>(use.getOwner()->getLoc(), dotOpType, cvtOpNew.getResult());
-        use.set(dotOpValue);
-      }
     }
   }
 }
